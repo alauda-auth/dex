6cd31f5eaa6594435d94743431e3999aebcabe6a
wangchao <chaowang1@alauda.io>
2021-03-28 23:57:18 +0800

    update extend

--- storage/storage_extend.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-import "github.com/dexidp/dex/pkg/log"
-
-// StorageConfig is a configuration that can create a storage.
-type StorageConfig interface {
-	Open(logger log.Logger) (Storage, error)
-}
